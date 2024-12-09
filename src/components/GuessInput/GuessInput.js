66705d2bf57adb3c3e12db801fc624da6cc92246
bmacmill <bnobile@gmail.com>
2024-12-09 16:45:31 -0500

    add input component
    added input component; met all requirements

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "../../styles.css"
+
+function GuessInput() {
+  const [inputText, setInputText] = React.useState('')
+
+  return <form className="guess-input-wrapper" onSubmit={((e) => {
+    e.preventDefault()
+    console.log({ inputText })
+    setInputText("")
+  })
+  }>
+    <label htmlFor="guess-input">Enter guess:</label>
+    <input id="guess-input" type="text" pattern="[A-Za-z]{5}" maxLength="5"
+      value={inputText}
+      disable={inputText.length < 4 ? "true" : "false"}
+      onChange={(e) => {
+        const upperGuess = e.target.value.toUpperCase()
+        setInputText(upperGuess)
+      }} />
+  </form >
+
+}
+
+export default GuessInput;
